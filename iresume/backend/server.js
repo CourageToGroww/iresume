c7452c2c45052e35b0f0a26f45568141f308aa04
CourageToGroww <monkeysbuymainstream@gmail.com>
2022-06-16 10:38:38 -0500

    creating server

---
+++ iresume/backend/server.js
@@ -0,0 +1,15 @@
+import express from 'express'
+import cors from 'cors' //Cross-Origin Resource Sharing
+import work_history from '.api/work_history.route.js'
+
+const app=express()
+
+app.use(cors())
+app.use(express.json())
+
+app.use("/api/v1/work_history",work_history)
+app.use('*',(req,res)=>{
+    res.status(404).json({error: "not found"})
+})
+
+export default app
